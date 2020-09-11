b9a69dc5c1f9ba902fbee23c62eac0ce77f9d17f
Raj Kumar <interplaytheheads@gmail.com>
2020-09-11 20:09:06 +0200

    Some links created for the display purpose

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import './Register_style.css';
+//import Index from './anindex';
+//import About from './About';
+//import Users from './users';
+//import FormPage from './Register';
 
-function App() {
+
+function AppRouter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/anindex">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+            <li>
+              <Link to="/Register/">Register</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="./anindex"  />
+         </div>
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
